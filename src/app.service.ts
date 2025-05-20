fd382851a1361177cd4cb242c8fe1857bbb78942
Hy3ons <hhs200306@gmail.com>
2025-05-21 00:20:47 +0900

    fix : 리다이렉트 오류 수정 및 앱 서비스 기능 삭제

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,4 @@ import { Injectable, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Injectable()
-export class AppService {
-  getHome(@Res() res: Response) {
-    res.redirect('https://github.com/QnoteAITeam');
-  }
-}
+export class AppService {}
